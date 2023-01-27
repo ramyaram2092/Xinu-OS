ccc0635e27accfae7581c487ccaf6270241f5970
Ramya Non-IU <ramyaram2092@outlook.com>
2023-01-26 19:23:52 -0500

    exception handling

--- shell/xsh_prodcons.c
+++ shell/xsh_prodcons.c
@@ -2,13 +2,32 @@
 #include<stdio.h>
 #include<prodcons.h>
 #include<string.h>
+#include <ctype.h>
 
 
 shellcmd xsh_prodcons(int nargs, char *args[])
 {
     int count;
-    // printf("nargs]: %d\n ",nargs);
-    if(nargs==1)
+    
+    // check if the argument  passed is a number 
+    char s[]= args[1];
+    if(nargs==2)
+    {
+        int i=0;
+        while(i<strlen(s))
+        {
+            if(!isDigit(s[i]))
+            {
+                printf("Not a number \n");
+                return 0;
+            }
+            i++;
+        }
+    }
+
+
+    // if there are no argument passed 
+    else if(nargs==1)
     {
         count=2000;
     }
@@ -17,15 +36,8 @@ shellcmd xsh_prodcons(int nargs, char *args[])
         count=atoi(args[1]);
     }
 
-    if(!isdigit(count))
-    {
-        printf("Not a number\n");
-        exit(1);
-    }
-
-    resume(create(producer,1024,20, "producer",1,count));
-    resume(create(consumer,1024,20,"consumer",1,count));
-
+        resume(create(producer,1024,20, "producer",1,count));
+        resume(create(consumer,1024,20,"consumer",1,count));
 
     return 0;
 }
