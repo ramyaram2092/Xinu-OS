fdfe43cd434b77692c00f10bc74d7271ee23e627
Ramya Non-IU <ramyaram2092@outlook.com>
2023-02-01 21:15:28 -0500

    Initial logic in

--- shell/xsh_run.c
+++ shell/xsh_run.c
@@ -1,9 +1,33 @@
-#include<xinu.h>
-#include<stdio.h>
+#include <xinu.h>
+#include <stdio.h>
+#include <shprototypes.h>
+
+// shellcmd xsh_prodcons(int32, char *[]);
+// shellcmd xsh_hello	(int32, char *[]);
 
 
-shellcmd xsh_hello(int nargs, char *args[])
-{
-    printf("Hello %s, Welcome to the world of Xinu!!",args[1]);
+
+shellcmd xsh_run(int nargs, char *args[])
+{   
+    // print the list of known commands 
+    if(nargs==1 || strncmp(args[1],"list",4)==0)
+    {
+        printf("hello\n");
+        printf("prodcons\n");
+    }
+    // execute hello command
+    else if(strncmp(args[1],"hello")==0)
+    {
+        resume (create((void *) xsh_hello, 4096, 20, "hello", 2, nargs - 1, &(args[1])));
+
+    }
+
+    //execute prodcons command
+    else if(strncmp(args[1],"prodcons")==0)
+    {
+        resume (create((void *) xsh_prodcons, 4096, 20, "prodcons", 2, nargs - 1, &(args[1])));
+
+    }
+    
     return 0;
 }
