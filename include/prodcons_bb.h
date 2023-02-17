98067380741aeb1fea4bc9e36312a0d34f47b6fa
Ramya Non-IU <ramyaram2092@outlook.com>
2023-02-16 22:48:42 -0500

    syntax error fix

--- include/prodcons_bb.h
+++ include/prodcons_bb.h
@@ -2,5 +2,5 @@ extern int arr_q[5];
 extern int head, tail, total;
 extern sid32 lock;
 
-void consumer_bb(int count);
-void producer_bb(int count);
+void consumer_bb(int count, char s[]);
+void producer_bb(int count, char s[]);
