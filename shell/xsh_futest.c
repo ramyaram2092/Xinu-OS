f6474b222558bf8ad8663c863ce8951beb019abe
Ramya Non-IU <ramyaram2092@outlook.com>
2023-03-02 19:41:03 -0500

    Initial commit for a5 : fixing syntax erroes

--- shell/xsh_futest.c
+++ shell/xsh_futest.c
@@ -48,11 +48,11 @@ shellcmd xsh_futest(int nargs, char *args[])
     {
         if (args[i] == isnumber(args[i]))
         {
-            resume(create(produce_fut, 1024, 20, "producer_fut", 2, atoi(args[i]), f));
+            resume(create(producer_fut, 1024, 20, "producer_fut", 2, atoi(args[i]), f));
         }
         else if (args[i] == "g")
         {
-            resume(create(produce_fut, 1024, 20, "consumer_fut", 1, f->data));
+            resume(create(consumer_fut, 1024, 20, "consumer_fut", 1, f));
         }
     }
 
