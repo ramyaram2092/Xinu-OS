63d19fe4c23edb413ac9e6e0f15bd5d63a8bdbc7
Ramya Non-IU <ramyaram2092@outlook.com>
2023-03-03 06:29:22 -0500

    clean up  code  and fixing bugswithout the wrapper function

--- shell/xsh_futest.c
+++ shell/xsh_futest.c
@@ -31,7 +31,7 @@ shellcmd xsh_futest(int nargs, char *args[])
     {
         for (int i = 1; i < nargs; i++)
         {
-            if ( !(!isnumber(args[i]) || args[i][0] != 'g'))
+            if ( !(isnumber(args[i]) || strncmp(args[i],"g",1)==0))
             {
                 printf("Invalid arguments \n");
                 // signal(completecmd);
@@ -52,7 +52,7 @@ shellcmd xsh_futest(int nargs, char *args[])
         {
             resume(create(producer_fut, 1024, 20, "producer_fut", 2, atoi(args[i]), f1));
         }
-        else if (args[i][0] == 'g')
+        else if (args[i] == "g")
         {
             resume(create(consumer_fut, 1024, 20, "consumer_fut", 1, f1));
         }
