1fd24c96760db60b43e53287ef2c421b195dd5a1
Ramya Non-IU <ramyaram2092@outlook.com>
2023-03-02 21:41:05 -0500

    debug future producerlogic

--- apps/producer_fut.c
+++ apps/producer_fut.c
@@ -7,8 +7,7 @@
 void producer_fut(int val,future_t* f)
 {
     int v=val;
-
     // set the value to future 
     future_set(f,&v);
-    printf("Producing:%d\n", f->data);
+    printf("Producing:%d\n",*(int*) f->data);
 }
