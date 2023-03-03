3cc0dcd1c3cb4ab251e42a027078cde517d7aa23
Ramya Non-IU <ramyaram2092@outlook.com>
2023-03-03 01:44:23 -0500

    verifying createfut

--- system/createfut.c
+++ system/createfut.c
@@ -20,9 +20,9 @@ future_t *future_alloc(future_mode_t mode, uint size, uint nelems)
     // allocate size for the data
     f2.data=(void *)getmem(size);
 
-    int v=10;
+    // int v=10;
 
-    f2.data=&v;
+    *f2.data=11;
     printf("Set the vlue during create :%d\n", *(int*)f2.data);
     
     
