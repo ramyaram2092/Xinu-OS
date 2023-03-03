f4c11b1277e6842f541fb3f927769f0671637e63
Ramya Non-IU <ramyaram2092@outlook.com>
2023-03-03 03:40:18 -0500

    debug: global/local variable in consumer

--- apps/consumer_fut.c
+++ apps/consumer_fut.c
@@ -7,9 +7,11 @@
 void consumer_fut(future_t * f)
 {
   printf("\n IN CONSUMER PROCESS\n");
+     printf("Address of future data : %d\n",f->data);
+        printf("Value of future data :%d\n", *(int*)(f->data));
   void* p=&value;
-  
-  printf("Address of value before calling futget : %d\n",p);
+
+  // printf("Address of value before calling futget : %d\n",p);
 
   // return the value that the future holds
   future_get(f,p);
