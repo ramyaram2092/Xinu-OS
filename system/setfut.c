acba9b471a065840e002c218442d70339398c404
Ramya Non-IU <ramyaram2092@outlook.com>
2023-03-02 21:47:37 -0500

    debug future producerlogic

--- system/setfut.c
+++ system/setfut.c
@@ -18,7 +18,7 @@ syscall future_set(future_t *f, void *in)
    if(f->state==FUTURE_EMPTY )
    {
         f->state=FUTURE_READY;
-        f->data=in;
+        f->data=*(int*)in; // write the value in the future
         return OK;
    }
    else
