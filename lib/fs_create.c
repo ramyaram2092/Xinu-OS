b722db32c4ab08a6db79d15cac32c024797c0d46
Ramya Non-IU <ramyaram2092@outlook.com>
2023-04-24 18:08:24 -0400

    initial commit for fs_create.c

---
+++ lib/fs_create.c
@@ -0,0 +1,19 @@
+#include <xinu.h>
+#include <fs.h>
+
+extern fsystem_t* fsd;
+
+/*
+ * fs_create - Return SYSERR if not enough space is available
+ *     in the directory or the filename already exists.
+ *     Otherwise:
+ *        1. Find an available block on the block store
+ *        2. Create an inode_t for the new file
+ *        3. Update the root directory
+ *        4. Write the inode and free bitmask back to the block device
+ */
+syscall fs_create(char* filename) {
+  
+  
+  return OK;
+}
