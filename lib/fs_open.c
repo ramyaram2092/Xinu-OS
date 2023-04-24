fe68f4c90a2d1b09ba3751531e4a32731dfc9a40
Ramya Non-IU <ramyaram2092@outlook.com>
2023-04-24 18:09:02 -0400

    initial commit for fs_open.c

---
+++ lib/fs_open.c
@@ -0,0 +1,19 @@
+
+#include <xinu.h>
+#include <fs.h>
+
+extern fsystem_t* fsd;
+extern filetable_t oft[NUM_FD];
+
+/*
+ * fs_open - Return SYSERR if the file is already open or does not exist
+ *     otherwise, add a record to the oft in the first current FSTATE_CLOSED
+ *     slot corresponding to the opened file and copy the inode from the block
+ *     device to the oft record and return the index into the oft table for the
+ *     newly opened file.
+ */
+int fs_open(char* filename, int flags) {
+
+  
+  return OK;
+}
