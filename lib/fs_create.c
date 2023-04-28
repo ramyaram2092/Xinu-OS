b4aafb7a954bff01c0ce8117aa3a1590d96623b9
Ramya Non-IU <ramyaram2092@outlook.com>
2023-04-28 14:35:42 -0400

    Debug: Initial logic for create file

--- lib/fs_create.c
+++ lib/fs_create.c
@@ -14,7 +14,7 @@ extern fsystem_t *fsd;
  */
 syscall fs_create(char *filename)
 {
-  intmask mask=disable();
+  intmask mask = disable();
   int freeb = 0;                 // free block index
   directory_t r = fsd->root_dir; // root directory reference
 
@@ -31,7 +31,7 @@ syscall fs_create(char *filename)
     freeb++;
   }
 
-  printf("\n FIRST AVILABLE FREE BLOCK : %d", freeb);
+  // printf("\n FIRST AVILABLE FREE BLOCK : %d", freeb);
 
   // 2. Return SYSERR if not enough space is available
   if (freeb == fsd->freemasksz)
@@ -54,35 +54,42 @@ syscall fs_create(char *filename)
   // 4. create inode_t for the new file
 
   inode_t in;
-  in.id = 100;
+  in.id = 1;
+  for (int i = 0; i < DIR_SIZE; i++)
+  {
+    in.blocks[i]=0;
+  }
 
   // 5. Write the inode and free bitmask back to the block device
 
+  // mark the block as used
   fs_setmaskbit(freeb);
+
+  // add the inode details in the directory entries
   for (int i = 0; i < DIR_SIZE; i++)
   {
-    if(r.entry[i].inode_block!=0)
+    if (r.entry[i].inode_block != 0)
     {
-      r.entry[i].inode_block=freeb;
-      int j=0;
-      while(j<FILENAME_LEN)
+      r.entry[i].inode_block = freeb;
+      int j = 0;
+      while (j < FILENAME_LEN)
       {
-        r.entry[i].name[j]=filename;
+        r.entry[i].name[j] = filename;
         j++;
         filename++;
       }
     }
   }
 
-  void *buffer= getmem(sizeof(inode_t));
+ // write the inode into the block assigned for the inode 
+  void *buffer = getmem(sizeof(inode_t));
 
   memcpy(buffer, &in, sizeof(inode_t));
-  
-  inode_t* cpy=(inode_t *)buffer;
-  
 
-  printf("\n COPIED VALUE OF BUFFER INODE ID : %d",cpy->id);
-  bs_write(freeb,0,buffer,sizeof(buffer));
+  inode_t *cpy = (inode_t *)buffer;
+
+  // printf("\n COPIED VALUE OF BUFFER INODE ID : %d", cpy->id);
+  bs_write(freeb, 0, buffer, sizeof(buffer));
   restore(mask);
   return OK;
 }
