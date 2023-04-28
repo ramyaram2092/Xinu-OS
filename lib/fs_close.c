60940d3e778b185626a5dda0c5514302d7181fa1
Ramya Non-IU <ramyaram2092@outlook.com>
2023-04-28 17:48:51 -0400

    updated logic for close

--- lib/fs_close.c
+++ lib/fs_close.c
@@ -2,44 +2,31 @@
 #include <xinu.h>
 #include <fs.h>
 
-extern fsystem_t* fsd;
+extern fsystem_t *fsd;
 extern filetable_t oft[NUM_FD];
-
 
 /*
  * fs_close - Return SYSERR if the file is already closed
  *     otherwise, write the file's inode back to the block
  *     device and set the entry in the oft to FSTATE_CLOSED
  */
-syscall fs_close(int fd) {
+syscall fs_close(int fd)
+{
   intmask mask = disable();
 
-  int i=0,flag=0;
-  for(i=0;i<NUM_FD;i++)
-  {
-    if(oft[i].in.id==fd && oft[i].state==FSTATE_CLOSED)
-    {
-        restore(mask);
+  int i = 0;
 
-      return SYSERR;
-    }
-    else if(oft[i].in.id==fd)
-    {
-      void* buffer= &oft[i].in;
-      bs_write(fd,0,buffer,sizeof(buffer));
-      oft[i].state=FSTATE_CLOSED;
-      oft[i].fileptr=0;
-      flag=1;
-      break;
-    }
-  }
-
-  if(i==NUM_FD && flag==0)
+  if (oft[fd].state == FSTATE_CLOSED)
   {
-      restore(mask);
+    restore(mask);
     return SYSERR;
   }
-  
+
+  void *buffer = (void *)&oft[fd].in;
+  bs_write(oft[fd].in.id, 0, buffer, sizeof(buffer));
+  oft[fd].state = FSTATE_CLOSED;
+  oft[fd].fileptr = 0;
+
   restore(mask);
   return OK;
 }
