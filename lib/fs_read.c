b315965d59ff9dc1ce0e63054c92a829711f6101
Ramya Non-IU <ramyaram2092@outlook.com>
2023-04-29 02:39:21 -0400

    Initail logic for read

--- lib/fs_read.c
+++ lib/fs_read.c
@@ -1,7 +1,7 @@
 #include <xinu.h>
 #include <fs.h>
 
-extern fsystem_t* fsd;
+extern fsystem_t *fsd;
 extern filetable_t oft[NUM_FD];
 
 /*
@@ -14,6 +14,54 @@ extern filetable_t oft[NUM_FD];
  *    - Update the fileptr field
  *    - Return the number of bytes read
  */
-int fs_read(int fd, char* buff, int len) {
-  return 0;
+int fs_read(int fd, char *buff, int len)
+{
+
+  // get the inode
+  inode_t inode = oft[fd].in;
+
+  // file ptr
+
+   oft[fd].fileptr=0;
+
+  // local buffer  for inode
+  void *ibuffer = getmem(sizeof(inode_t));
+
+  // no of blocks
+  int maxblck = inodeb.size / 512;
+
+  //no of bytes read
+  int rbytes=0;
+
+  if (len > 512)
+  {
+    size = 512;
+  }
+  else
+  {
+    size = len;
+  }
+
+  int i = 0;
+  while (size > 0 && i <= maxblck)
+  {
+    offset=oft[fd].fileptr;
+    // read the data from the block device into buff
+    int didx = inode.blocks[i];
+    bs_read(didx, offset, buff, size);
+    // increment the buff pointer
+    buff += size;
+
+    rbytes+=size;
+
+    // update the size
+    size = size - len;
+
+    // update the filepointer
+    oft[fd].filept+=size;
+
+    i++;
+  }
+
+  return rbytes;
 }
